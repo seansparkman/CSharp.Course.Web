a090ea9d8122b051ba32393439ee4a14290dc270
Sean Sparkman <sean.sparkman@outlook.com>
2017-04-04 16:18:38 -0500

    sort order in one place as well as top ten

--- CSharp.Course.Web/Models/LeaderboardContext.cs
+++ CSharp.Course.Web/Models/LeaderboardContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Linq;
 
 namespace CSharp.Course.Web.Models
 {
@@ -13,5 +14,15 @@ namespace CSharp.Course.Web.Models
         }
 
         public DbSet<BoardEntry> Leaderboard { get; set; }
+
+        public IOrderedQueryable<BoardEntry> SortedLeaderboard
+        {
+            get { return Leaderboard.OrderBy(be => be.Passed).ThenBy(be => be.Submitted).ThenBy(be => be.Username); }
+        }
+
+        public IQueryable<BoardEntry> TopTenEntries
+        {
+            get { return SortedLeaderboard.Take(10); }
+        }
     }
 }
