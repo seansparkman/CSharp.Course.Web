81a5fd6fc948ada827fbf138071bf6fc0e15fb85
Sean Sparkman <sean.sparkman@outlook.com>
2017-04-04 16:05:14 -0500

    add update event if the top 10 changes

--- CSharp.Course.Web/Controllers/LeaderboardController.cs
+++ CSharp.Course.Web/Controllers/LeaderboardController.cs
@@ -1,5 +1,7 @@
-﻿using CSharp.Course.Web.Models;
+﻿using CSharp.Course.Web.Hubs;
+using CSharp.Course.Web.Models;
 using CSharp.Course.Web.Models.Dto;
+using Microsoft.AspNet.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -43,6 +45,24 @@ namespace CSharp.Course.Web.Controllers
                 // add new entry
                 var result = dbContext.Leaderboard.Add(boardEntry);
                 await dbContext.SaveChangesAsync();
+
+                if (dbContext.Leaderboard.OrderBy(eb => eb.Passed).ThenBy(eb => eb.Username).Any(be => be.Id == result.Id))
+                {
+                    var boardEntries = from entry in dbContext.Leaderboard.
+                                            OrderBy(eb => eb.Passed).ThenBy(eb => eb.Username).
+                                            Take(10)
+                                       select new BoardEntryDto
+                                       {
+                                           Id = entry.Id,
+                                           Failed = entry.Failed,
+                                           Passed = entry.Passed,
+                                           Skipped = entry.Skipped,
+                                           Username = entry.Username
+                                       };
+
+                    var clients = GlobalHost.ConnectionManager.GetHubContext<LeaderboardHub>().Clients;
+                    clients.All.leaderboardUpdate(boardEntries.ToListAsync());
+                }
 
                 return new BoardEntryDto
                 {
