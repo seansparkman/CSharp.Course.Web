9b3bc393114cb333867ca2f89fe61dafa1c2fa52
Sean Sparkman <sean.sparkman@outlook.com>
2017-04-04 14:51:31 -0500

    add model validation to dto object

--- CSharp.Course.Web/Models/Dto/BoardEntryDto.cs
+++ CSharp.Course.Web/Models/Dto/BoardEntryDto.cs
@@ -1,13 +1,18 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace CSharp.Course.Web.Models.Dto
 {
     public class BoardEntryDto
     {
         public Guid Id { get; set; }
+        [Required, MaxLength(50)]
         public string Username { get; set; }
+        [Required, Min(0)]
         public int Passed { get; set; }
+        [Required, Min(0)]
         public int Failed { get; set; }
+        [Required, Min(0)]
         public int Skipped { get; set; }
     }
 }
