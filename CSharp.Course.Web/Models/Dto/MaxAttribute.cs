2004c267e8f96046ffd547c38cddc6acfcb4c58e
Sean Sparkman <sean.sparkman@outlook.com>
2017-04-04 14:46:52 -0500

    add min and max attributes

---
+++ CSharp.Course.Web/Models/Dto/MaxAttribute.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CSharp.Course.Web.Models.Dto
+{
+    public class MaxAttribute : ValidationAttribute
+    {
+        public MaxAttribute(int max)
+        {
+            Max = max;
+        }
+
+        public MaxAttribute(int max, string errorMessage)
+            : base(errorMessage)
+        {
+            Max = max;
+        }
+
+        protected MaxAttribute(string errorMessage) : base(errorMessage)
+        {
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value is int)
+            {
+                var number = (int)value;
+
+                return number < Max;
+            }
+
+            return false;
+        }
+
+        public int Max { get; private set; }
+    }
+}
---
+++ CSharp.Course.Web/Models/Dto/MinAttribute.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CSharp.Course.Web.Models.Dto
+{
+    public class MinAttribute : ValidationAttribute
+    {
+        public MinAttribute(int min)
+        {
+            Min = min;
+        }
+
+        public MinAttribute(int min, string errorMessage)
+            : base(errorMessage)
+        {
+            Min = min;
+        }
+
+        protected MinAttribute(string errorMessage) : base(errorMessage)
+        {
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value is int)
+            {
+                var number = (int)value;
+
+                return number < Min;
+            }
+
+            return false;
+        }
+
+        public int Min { get; private set; }
+    }
+}
