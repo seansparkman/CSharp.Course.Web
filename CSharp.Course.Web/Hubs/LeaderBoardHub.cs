693b9157946a6e2652b1abd6c5075436a2b43c70
Sean Sparkman <sean.sparkman@outlook.com>
2017-04-04 15:53:04 -0500

    created leaderboard signalr hub

---
+++ CSharp.Course.Web/Hubs/LeaderBoardHub.cs
@@ -0,0 +1,35 @@
+﻿using CSharp.Course.Web.Models;
+using CSharp.Course.Web.Models.Dto;
+using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CSharp.Course.Web.Hubs
+{
+    public class LeaderboardHub : Hub
+    {
+        public async Task<List<BoardEntryDto>> GetBoard()
+        {
+            using (var dbContext = new LeaderboardContext())
+            {
+                var boardEntries = from entry in dbContext.Leaderboard.
+                                        OrderBy(eb => eb.Passed).ThenBy(eb => eb.Username).
+                                        Take(10)
+                                   select new BoardEntryDto
+                                   {
+                                       Id = entry.Id,
+                                       Failed = entry.Failed,
+                                       Passed = entry.Passed,
+                                       Skipped = entry.Skipped,
+                                       Username = entry.Username
+                                   };
+
+                return await boardEntries.ToListAsync();
+            }
+        }
+    }
+}
