116aed4a13a69dceb71efbdc90e7e465600a5a9f
Sean Sparkman <sean.sparkman@outlook.com>
2017-04-04 19:16:56 -0500

    setup leaderboad to ajax request

---
+++ CSharp.Course.Web/Scripts/leaderboard.js
@@ -0,0 +1,52 @@
+﻿(function () {
+    $.ajax('/api/leaderboard', {
+        dataType: 'json',
+        success: function (data, status) {
+            var entityMap = {
+                '&': '&amp;',
+                '<': '&lt;',
+                '>': '&gt;',
+                '"': '&quot;',
+                "'": '&#39;',
+                '/': '&#x2F;',
+                '`': '&#x60;',
+                '=': '&#x3D;'
+            };
+
+            for (var index = 1; index <= 10; index++) {
+                var tableRow = '<tr><td>' + index + '</td>';
+
+                if (data.length > index - 1)
+                {
+                    var item = data[index - 1];
+                    tableRow += '<td>' + item.Username.replace(/[&<>"'`=\/]/g, function (s) {
+                        return entityMap[s];
+                    }) + '</td>';
+                    tableRow += '<td>' + item.Passed + '</td>';
+                    tableRow += '<td>' + item.Skipped + '</td>';
+                    tableRow += '<td>' + item.Failed + '</td>';
+                }
+
+                tableRow += '</tr>';
+                $('#leaderboard tr:last').after(tableRow);
+            }
+
+
+            var leaderboardHubProxy = $.connection.leaderboardHub;
+            leaderboardHubProxy.client.leaderboardUpdate = function (board) {
+                console.log(JSON.stringify(board));
+            };
+            $.connection.hub.start()
+                .done(function () { console.log('Now connected, connection ID=' + $.connection.hub.id); })
+                .fail(function () { console.log('Could not Connect!'); });
+        },
+        error: function () {
+            $('#errorMessage').show();
+        }
+    });
+
+
+
+
+
+})();
