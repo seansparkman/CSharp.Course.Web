d214c64e23151f7791488543e63b1f4a9ab23ab6
Sean Sparkman <sean.sparkman@outlook.com>
2017-04-04 19:25:29 -0500

    populate the table as the board gets updated

--- CSharp.Course.Web/Scripts/leaderboard.js
+++ CSharp.Course.Web/Scripts/leaderboard.js
@@ -1,40 +1,44 @@
 ﻿(function () {
+    function populateLeaderboard(board) {
+        var entityMap = {
+            '&': '&amp;',
+            '<': '&lt;',
+            '>': '&gt;',
+            '"': '&quot;',
+            "'": '&#39;',
+            '/': '&#x2F;',
+            '`': '&#x60;',
+            '=': '&#x3D;'
+        };
+
+        for (var index = 1; index <= 10; index++) {
+            var tableRow = '<tr><td>' + index + '</td>';
+
+            if (board.length > index - 1) {
+                var item = board[index - 1];
+                tableRow += '<td>' + item.Username.replace(/[&<>"'`=\/]/g, function (s) {
+                    return entityMap[s];
+                }) + '</td>';
+                tableRow += '<td>' + item.Passed + '</td>';
+                tableRow += '<td>' + item.Skipped + '</td>';
+                tableRow += '<td>' + item.Failed + '</td>';
+            }
+
+            tableRow += '</tr>';
+            console.log(tableRow);
+            $('#leaderboard tr:last').after(tableRow);
+        }
+    }
     $.ajax('/api/leaderboard', {
         dataType: 'json',
         success: function (data, status) {
-            var entityMap = {
-                '&': '&amp;',
-                '<': '&lt;',
-                '>': '&gt;',
-                '"': '&quot;',
-                "'": '&#39;',
-                '/': '&#x2F;',
-                '`': '&#x60;',
-                '=': '&#x3D;'
-            };
-
-            for (var index = 1; index <= 10; index++) {
-                var tableRow = '<tr><td>' + index + '</td>';
-
-                if (data.length > index - 1)
-                {
-                    var item = data[index - 1];
-                    tableRow += '<td>' + item.Username.replace(/[&<>"'`=\/]/g, function (s) {
-                        return entityMap[s];
-                    }) + '</td>';
-                    tableRow += '<td>' + item.Passed + '</td>';
-                    tableRow += '<td>' + item.Skipped + '</td>';
-                    tableRow += '<td>' + item.Failed + '</td>';
-                }
-
-                tableRow += '</tr>';
-                $('#leaderboard tr:last').after(tableRow);
-            }
-
+            populateLeaderboard(data);
 
             var leaderboardHubProxy = $.connection.leaderboardHub;
             leaderboardHubProxy.client.leaderboardUpdate = function (board) {
                 console.log(JSON.stringify(board));
+                $('#leaderboard ').find('tr:gt(0)').remove();
+                populateLeaderboard(board);
             };
             $.connection.hub.start()
                 .done(function () { console.log('Now connected, connection ID=' + $.connection.hub.id); })
